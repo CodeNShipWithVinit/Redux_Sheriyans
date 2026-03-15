4f3ea4d5f34d55dd789239604679ffbb2b4372b8
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2026-03-16 02:28:54 +0530

    Created the file structure and implemented the searchSlice

--- Redux_GifSpace/src/App.jsx
+++ Redux_GifSpace/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { fetchPhotos, fetchVideos } from './api/mediaApi'
 
 const App = () => {
   return (
-    <div className='bg-red-500'>App</div>
+    <div className='h-screen w-full text-white bg-gray-950'>App
+      <button className='bg-green-400 p-3 m-2' onClick={async()=>{
+        const data=await fetchPhotos("cat");
+        console.log(data.results);
+      }}>Get Photos</button>
+
+
+      <button className='bg-green-400 p-3 m-2' onClick={async()=>{
+        const data=await fetchVideos("cat");
+        console.log(data.videos);
+      }}>Get Videos</button>
+    </div>
   )
 }
 
